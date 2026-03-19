03f6523a131bf9d58d8a8fe5c8b18d0d6a4349c7
Fratsia <fauzantrisuladana@gmail.com>
2026-03-19 16:16:08 +0700

    dari bu divi

---
+++ src/main/java/com/example/LibraryModel.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class LibraryModel {
+
+    public List<Book> getAllBooks() {
+
+        List<Book> allBooks = new ArrayList<>();
+        // Search in database
+        return allBooks;
+    }
+
+    void saveSearchKeyword(String keyword) {
+        // Save to db
+    }
+}
---
+++ src/test/java/com/example/LibraryTest.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class LibraryTest {
+
+    @Mock
+    private LibraryModel bookModel;
+
+    @InjectMocks
+    private LibraryControl control;
+
+    @Test
+    public void testSearchBooksAuthor() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1, "Intro OOP", "divi"));
+        books.add(new Book(2, "Pengujian PL itu mudah", "putri"));
+
+        // stub
+        Mockito.when(bookModel.getAllBooks()).thenReturn(books);
+
+        Assertions.assertEquals("divi", control.SearchBooksAuthor("Intro OOP"));
+
+        // mock 
+        Mockito.verify(bookModel, Mockito.times(1)).getAllBooks();
+    }
+
+    @Test
+    public void testSearchBooksAuthor_withNever() {
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1, "Intro OOP", "divi"));
+        
+        // stub
+        Mockito.when(bookModel.getAllBooks()).thenReturn(books);
+        control.SearchBooksAuthor("Unknown Book");
+
+        Mockito.verify(bookModel, Mockito.never()).saveSearchKeyword(Mockito.anyString());
+    }
+}
